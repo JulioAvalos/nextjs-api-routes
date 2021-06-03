bc015a9982069fbd8ba68d53e50dad6ca4ca2a99
JulioAvalos <julioavalos2@msn.com>
2021-06-02 23:38:09 -0600

    using API Routes for Pre-Rendering Pages

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,11 +1,11 @@
 import fs from "fs";
 import path from "path";
 
-function buildFeedbackPath() {
+export function buildFeedbackPath() {
   return path.join(process.cwd(), "data", "feedback.json");
 }
 
-function extractFeedback(filePath) {
+export function extractFeedback(filePath) {
   const fileData = fs.readFileSync(filePath);
   const data = JSON.parse(fileData);
   return data;
@@ -30,7 +30,7 @@ function handler(req, res) {
   } else {
     const filePath = buildFeedbackPath();
     const data = extractFeedback(filePath);
-    res.status(200).json({ feedback: data });
+    res.status(200) .json({ feedback: data });
   }
 }
 
