57e3e785fab0735e39ca7ae236c58101476da0b3
JulioAvalos <julioavalos2@msn.com>
2021-06-02 19:53:23 -0600

    -

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -5,7 +5,6 @@ function handler(req, res) {
   if (req.method === "POST") {
     const email = req.body.email;
     const feedbackText = req.body.text;
-
     const newFeedback = {
       id: new Date().toISOString(),
       email: email,
@@ -20,7 +19,6 @@ function handler(req, res) {
     fs.writeFileSync(filePath, JSON.stringify(data));
     res.status(201).json({ message: "Success!", feedback: newFeedback });
   } else {
-    console.log(' se fue alv')
     res.status(200).json({ message: "This works!" });
   }
 }
